3f0a955a6fe05c4cee5008b9ecefdb5518289e23
olga.borodina <sensation65@gmail.com>
2019-10-20 19:07:44 +0700

    Добавлен файл conftest

--- fixture/application.py
+++ fixture/application.py
@@ -9,8 +9,8 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-        self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from model.contact import Contact
+from model.contact import ContactHelper
 
 def test_add_contact(app):
     app.session.login(username = "admin", password = "secret")
-    app.contact.client_create(Contact(firstname = "Petrov", middlename = "Petr", lastname = "Petrovich", nickname = "Ppp1", title = "Rere", company = "OooPetrov", address = "Petrova1-42",
+    app.contact.client_create(ContactHelper(firstname = "Petrov", middlename = "Petr", lastname = "Petrovich", nickname = "Ppp1", title = "Rere", company = "OooPetrov", address = "Petrova1-42",
                            home = "+7(333)9983333", mobile = "+7(912)8838833", work = "+7(938)8848844", fax = "+7(448)8847777", email = "dfdsf@rr.ru",
                            email2 = "ytytyt@tt.ru", email3 = "iiror@it.ru", homepage = "www.tt.ru", bday = "13", bmonth = "January", byear = "1990", aday = "15", amonth = "November",
                            ayear = "2015", address2 = "gfgdfg,3 - 5", phone2 = "dfdf", notes = "dfdfs"))
