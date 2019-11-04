4a4053a52931d3b037bd208c613b604eabb8abba
olga.borodina <sensation65@gmail.com>
2019-11-04 16:39:28 +0700

    Добавлены методы contact_fill_form и group_fill_form, изменен локатор

--- fixture/group.py
+++ fixture/group.py
@@ -13,15 +13,7 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.group_fill_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -42,6 +34,17 @@ class GroupHelper:
         # modification group
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
+        self.group_fill_form(group)
+        # save group
+        wd.find_element_by_name("update").click()
+        wd.find_element_by_link_text("group page").click()
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
+
+    def group_fill_form(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -51,10 +54,3 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # save group
-        wd.find_element_by_name("update").click()
-        wd.find_element_by_link_text("group page").click()
-
-    def return_to_groups_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
