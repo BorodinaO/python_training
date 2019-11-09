3d0e335b4fb6bc5fa377268db8ce0fcc8429a019
olga.borodina <sensation65@gmail.com>
2019-11-09 17:52:18 +0700

    Доработки списков для модификации и добавления групп

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -4,6 +4,10 @@ def test_mod_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.mod_first_group(Group(name="One", header="Two", footer="Three"))
+    group = Group(name="One", header="Two", footer="Three")
+    group.id = old_groups[0].id
+    app.group.mod_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
