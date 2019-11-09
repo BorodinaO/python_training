74821a599d4a80822aacdee365072c85a09f686f
olga.borodina <sensation65@gmail.com>
2019-11-09 17:08:50 +0700

    Добавлены сравнени списков для групп

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -3,4 +3,7 @@ from model.group import Group
 def test_mod_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.mod_first_group(Group(name="One", header="Two", footer="Three"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
