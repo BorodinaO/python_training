2f2d427ad3e066fbe7b2a52eb745e7b82ad34d4e
olga.borodina <sensation65@gmail.com>
2019-11-16 20:50:00 +0700

    Задание 16. Добавлена передача параметров для браузера и url

--- fixture/application.py
+++ fixture/application.py
@@ -5,12 +5,20 @@ from fixture.contactfixture import ContactFixture
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.contact = ContactFixture(self)
         self.group = GroupHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -21,8 +29,9 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//input[@value='Login']")) > 0):
-            wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
+        #if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//input[@value='Login']")) > 0):
+            #wd.get("http://localhost/addressbook/")
 
     def destroy(self):
         self.wd.quit()
