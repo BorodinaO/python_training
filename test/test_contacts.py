5447b4c6ab084fe6638acf237defe6f1899f8129
olga.borodina <sensation65@gmail.com>
2019-11-16 15:29:33 +0700

    Задание 14. Rename

--- test/test_contacts.py
+++ test/test_contacts.py
@@ -1,7 +1,7 @@
 import re
 
 
-def test_phones_on_home_page(app):
+def test_contacts_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert sorted(contact_from_home_page.all_phones_from_home_page) == sorted(
