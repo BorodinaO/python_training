b7e0165ef9fb07d793ed0377fef4a0b41fd3ed26
olga.borodina <sensation65@gmail.com>
2019-11-18 20:58:42 +0700

    Задание 21. Пправка

--- test/test_contacts.py
+++ test/test_contacts.py
@@ -10,22 +10,8 @@ def test_contacts_on_home_page(app, db):
         assert contact_from_ui[index].firstname.replace(' ', '') == contact_from_db[index].firstname.replace(' ', '')
         assert contact_from_ui[index].lastname.replace(' ', '') == contact_from_db[index].lastname.replace(' ', '')
         assert contact_from_ui[index].address.replace(' ', '') == contact_from_db[index].address.replace(' ', '')
-        assert contact_from_ui[index].all_phones_from_home_page == "\n".join(
-            filter(lambda x: x != "",
-                   map(lambda x: clear(x),
-                       filter(lambda x: x is not None,
-                              [contact_from_db[index].home,
-                               contact_from_db[index].mobile,
-                               contact_from_db[index].work,
-                               contact_from_db[index].phone2]))))
-
-        '''def test_phones_on_contact_view_page(app):
-            contact_from_view_page = app.contact.get_contact_from_view_page(0)
-            contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-            assert contact_from_view_page.home == contact_from_edit_page.home
-            assert contact_from_view_page.work == contact_from_edit_page.work
-            assert contact_from_view_page.mobile == contact_from_edit_page.mobile
-            assert contact_from_view_page.phone2 == contact_from_edit_page.phone2'''
+        assert contact_from_ui[index].all_phones_from_home_page == merge_phones_like_on_home_page(
+            contact_from_db[index])
 
 
 def clear(s):
@@ -36,7 +22,7 @@ def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                       [contact.home, contact.work, contact.mobile,
+                                       [contact.home, contact.mobile, contact.work,
                                         contact.phone2]))))
 
 
